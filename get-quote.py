1bdfd845a77d25da71bfab5ee9f215a40d5c7787
iambashar <bashar38@iut-dhaka.edu>
2019-12-27 02:08:40 +0600

    Final Touch

--- get-quote.py
+++ get-quote.py
@@ -1,3 +1,4 @@
+#edited by iambashar
 import random
 
 def primary():
@@ -6,9 +7,19 @@ def primary():
  f = open("quotes.txt")
  quotes = f.readlines()
  f.close()
- last = len(quotes) - 1
+ 
+ last = len(quotes) - 2
  rnd = random.randint(0, last)
- print(quotes[rnd])
+ print(quotes[rnd].strip())
+ print(quotes[rnd+1].strip())
+
+ f = open("quotes.txt", "a")
+ i = input("Enter 'E' to add quotes:")
+ if i == 'E':
+   h = input("Enter quotes to insert in your fav list : ")
+   h += "\n"
+   f.write(h)
+ f.close()
 
 if __name__== "__main__":
  	primary()
